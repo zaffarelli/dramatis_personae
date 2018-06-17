84cbdf9beb9151f199b7a3665458b223ef3de4d4
zaffarelli <zaffarelli@senestre.net>
2018-06-18 01:16:57 +0200

    XLS: weaponrefs

--- collector/xls_collector.py
+++ collector/xls_collector.py
@@ -1,97 +1,128 @@
 from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
-from collector.models import Character
+from collector.models import Character,WeaponRef
 from datetime import datetime 
+from collector.fs_fics7 import minmax_from_dc
 
 
-
+def export_header(ws,heads,colwidths):
+  r = 2
+  for num,h in enumerate(heads,start=0):
+    ws.cell(column=num+1, row=r, value=h)
+    ws.column_dimensions[get_column_letter(num+1)].width = colwidths[num]
+    
 def export_to_xls(filename='dramatis_personae.xls'):
   """ XLS extraction of the Characters """
   wb = Workbook()
   dest_filename = filename
   ws = wb.active
+  # Abstract
   ws.title = 'Abstract'
-
-  ws.cell(column=1, row=2, value='Dramatis Personae')
-  ws.cell(column=1, row=3, value='%s'%(datetime.now()))
-
-
-  ws = wb.create_sheet('Characters')
-  character_items = Character.objects.all().order_by('full_name')
-  ws.cell(column=1, row=1, value='Dramatis Personae')
-  cnt = 2
-  ccc = 1
-  ws.cell(column=ccc, row=cnt, value='Name')
   ws.column_dimensions['A'].width = 40
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='RID')
   ws.column_dimensions['B'].width = 30
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Alliance')
-  ws.column_dimensions['C'].width = 30
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Rank')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Gender')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Species/Race')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Caste')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Birthdate')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Height')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='Weight')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='STR')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='CON')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='BOD')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='MOV')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='INT')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='WIL')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='TEM')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='PRE')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='TEC')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='REF')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='AGI')
-  ccc+=1
-  ws.cell(column=ccc, row=cnt, value='AWA')
-  ccc+=1
+  ws.cell(column=1, row=2, value='Source')
+  ws.cell(column=2, row=2, value='Dramatis Personae Collector')
+  ws.cell(column=1, row=3, value='Version')
+  ws.cell(column=2, row=3, value='0.5')
+  ws.cell(column=1, row=4, value='Release date')
+  ws.cell(column=2, row=4, value='%s'%(datetime.now()))
+  # Characters
+  ws = wb.create_sheet('Characters')
+  h = ['Name','RID','Entrance','Alliance','Rank','Gender','Species/Race','Caste','Birthdate','Height','Weight','STR','CON','BOD','MOV','INT','WIL','TEM','PRE','TEC','REF','AGI','AWA']
+  w = [40,30,30,50,20,20,20,20,20,20,20,10,10,10,10,10,10,10,10,10,10,10,10]
+  ws.cell(column=1, row=1, value='Dramatis Personae')
+  character_items = Character.objects.all().order_by('full_name')
+  export_header(ws,h,w)
   cnt = 3
   for c in character_items:
-    ws.cell(column=1, row=cnt, value='%s'%(c.full_name))
-    ws.cell(column=2, row=cnt, value='%s'%(c.rid))
-    ws.cell(column=3, row=cnt, value='%s'%(c.alliance))
-    ws.cell(column=4, row=cnt, value='%s'%(c.rank))
-    ws.cell(column=5, row=cnt, value='%s'%(c.gender))
-    ws.cell(column=6, row=cnt, value='%s'%(c.species))
-    ws.cell(column=7, row=cnt, value='%s'%(c.caste))
-    ws.cell(column=8, row=cnt, value='%s'%(c.birthdate))
-    ws.cell(column=9, row=cnt, value='%s'%(c.height))
-    ws.cell(column=10, row=cnt, value='%s'%(c.weight))
-    ws.cell(column=11, row=cnt, value='%d'%(c.PA_STR))
-    ws.cell(column=12, row=cnt, value='%d'%(c.PA_CON))
-    ws.cell(column=13, row=cnt, value='%d'%(c.PA_BOD))
-    ws.cell(column=14, row=cnt, value='%d'%(c.PA_MOV))
-    ws.cell(column=15, row=cnt, value='%d'%(c.PA_INT))
-    ws.cell(column=16, row=cnt, value='%d'%(c.PA_WIL))
-    ws.cell(column=17, row=cnt, value='%d'%(c.PA_TEM))
-    ws.cell(column=18, row=cnt, value='%d'%(c.PA_PRE))
-    ws.cell(column=19, row=cnt, value='%d'%(c.PA_TEC))
-    ws.cell(column=20, row=cnt, value='%d'%(c.PA_REF))
-    ws.cell(column=21, row=cnt, value='%d'%(c.PA_AGI))
-    ws.cell(column=22, row=cnt, value='%d'%(c.PA_AWA))
+    x = 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.full_name))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.rid))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.entrance))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.alliance))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.rank))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.gender))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.species))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.caste))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.birthdate))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.height))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.weight))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_STR))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_CON))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_BOD))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_MOV))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_INT))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_WIL))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_TEM))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_PRE))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_TEC))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_REF))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_AGI))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%d'%(c.PA_AWA))
+    x += 1
+    cnt += 1
+  ws = wb.create_sheet('Weapons')
+  h = ['Ref','Cat','WA','CO','AV','DC','cal.','MinMax','Str','RoF','Clip','RNG','RE','Cost','Description']
+  w = [40,10,5,5,5,10,15,15,5,5,5,5,5,8,30]
+  ws.cell(column=1, row=1, value='Dramatis Personae')
+  weaponref_items = WeaponRef.objects.all().order_by('category','damage_class')
+  export_header(ws,h,w)
+  cnt = 3
+  for c in weaponref_items:
+    x = 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.reference))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.category))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.weapon_accuracy))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.conceilable))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.availability))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.damage_class))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.caliber))
+    x += 1
+    dcmm = minmax_from_dc(c.damage_class)
+    ws.cell(column=x, row=cnt, value='%d-%d (%.02f)'%(dcmm[0],dcmm[1],((dcmm[0]+dcmm[1])/2)))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.str_min))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.rof))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.clip))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.rng))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.rel))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.cost))
+    x += 1
+    ws.cell(column=x, row=cnt, value='%s'%(c.description))
+    x += 1
     cnt += 1
   wb.save(filename = dest_filename)
