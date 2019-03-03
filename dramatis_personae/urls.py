9e758fbd0de2145da12864556835c5140d863896
zaffarelli <zaffarelli@senestre.net>
2019-03-03 14:00:01 +0100

    Removed debugtoolbar

--- dramatis_personae/urls.py
+++ dramatis_personae/urls.py
@@ -22,7 +22,7 @@ admin.site.site_header = "Dramatis Personae Administration"
 admin.site.site_title = "Dramatis Personae Administration portal"
 admin.site.index_title = "Welcome to Dramatis Personae. Be a good gamemaster."
 
-import debug_toolbar
+#import debug_toolbar
 
 urlpatterns = [
 	re_path('admin/', admin.site.urls),
