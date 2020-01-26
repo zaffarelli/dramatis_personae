e60afa045e4f025a8089255b24c77e4ae5dbbab8
zaffarelli@gmail.com <zaffarelli@gmail.com>
2020-01-26 13:10:41 +0100

    on dp

--- dramatis_personae/settings/prod.py
+++ dramatis_personae/settings/prod.py
@@ -18,6 +18,6 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'dp.sqlite3'),
     }
 }
