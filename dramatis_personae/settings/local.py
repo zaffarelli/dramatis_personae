8026a6c5ee6160ab45ec25cbc5e1da47b5327cee
Zaffarelli <zaffarelli@gmail.com>
2021-02-11 23:57:40 +0100

    added zotzgi host

--- dramatis_personae/settings/local.py
+++ dramatis_personae/settings/local.py
@@ -5,7 +5,7 @@
 """
 from .base import *
 
-ALLOWED_HOSTS = ['localhost', '192.168.0.70', '192.168.0.60', 'phasma', 'galliard']
+ALLOWED_HOSTS = ['localhost', '192.168.0.70', '192.168.0.60', '192.168.0.61', '192.168.0.90', 'phasma', 'galliard', 'zotzgi']
 
 SECRET_KEY = '6j@b*@a*k0-23vmk4@i%r@_5es5+8uy!23rl2+1^qx491898-b'
 DEBUG = True
